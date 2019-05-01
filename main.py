d0034aa9467578481b4f4df9ffdfd5dee87cd244
moesmatheus <moesmatheus@gmail.com>
2019-05-01 10:51:17 -0300

    added gan networks

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ class ANN():
     def __init__(
             self,
             network_sizes,
-            activation=None,
-            dropout=0,
+            activation = None,
+            dropout = 0,
             optimizer = 'adam',
             loss = 'mean_squared_error',
             metrics = None,
-            n_out = 1
+            activation_out = None,
+            n_out = 1,
+            softmax = True,
            ):
         
         #Pass elements from init
@@ -34,10 +36,10 @@ class ANN():
             self.model.add(keras.layers.Dense(i,activation = self.activation))
         
         #Create output layer
-        self.model.add(keras.layers.Dense(n_out))
+        self.model.add(keras.layers.Dense(n_out,activation = self.activation_out))
         
         #If logits regression create softmax
-        if n_out > 1:
+        if n_out > 1 and self.softmax:
             self.model.add(keras.layers.Softmax())
         
         #Compile model
@@ -167,7 +169,8 @@ class CNN():
             filters,
             kernels,
             pool_size = (1,1),
-            activation=None,
+            activation = None,
+            activation_conv = None,
             dropout=0,
             optimizer = 'adam',
             loss = 'mean_squared_error',
@@ -184,7 +187,7 @@ class CNN():
         #Create Convolution
         for f,k in zip(self.filters,self.kernels):
             self.model.add(keras.layers.Conv2D(
-                f, kernel_size= k ,activation = self.activation,))
+                f, kernel_size= k ,activation = self.activation_conv,))
      
         #Create MaxPooling
         self.model.add(keras.layers.MaxPooling2D(pool_size=self.pool_size))
@@ -639,10 +642,200 @@ class RNN_multistep():
         
         plt.show()
         
+
+#Function to plot in a grid
+def plot_grid(ims, shape, cmap = None):
+    
+    selection = np.random.choice( range(len(ims) ), shape ,replace=False)
+
+    plt.figure(1)
+    
+    n = 1
+    
+    for i in range( shape[0] ):
+        
+        for j in range( shape[1] ):
+            
+            plt.subplot(shape[0], shape[1], n)
+            
+            plt.axis('off')
+            
+            plt.imshow(ims[ selection[i][j] ], interpolation='nearest', cmap = cmap)
+            
+            n += 1
+            
+    
+    plt.show()        
         
         
+class GAN():
+    def __init__(
+        self, 
+        generator, 
+        discriminator,
+        optimizer
+        ):
+        #Pass elements from init
+        self.__dict__.update(locals())
+        #Make discriminator not trainable
+        self.discriminator.trainable = False
+        #Create gan joining discriminator and generator
+        self.gan = keras.models.Sequential(layers = [generator,discriminator])
+        #Compile model
+        self.gan.compile(loss='binary_crossentropy', optimizer=self.optimizer)
+        #Current iteration on model
+        self.round = 0
+        #Loss on generator
+        self.loss_gen = []
+        #Loss on discriminator
+        self.loss_disc = []
+        
+    def train_gan(self,
+                  noise_function,
+                  actual,
+                  smoothing = 1,
+                  verbose = 1,
+                  actual_reshape = None,
+                  epochs_discr = 1,
+                  epochs_gen = 1,
+                  size_disc = None,
+                  batch_size_disc = None,
+                  size_gen = None,
+                  batch_size_gen = None,
+                 ):
+        
+        self.__dict__.update(locals())
+        
+        ###Show round
+        if self.verbose == 1: print('Round: ', self.round)
+            
+        ###Select from actual (batch size)
+        if self.size_disc != None:
+            ind = np.random.choice(
+                range(len(self.actual)),self.size_disc, replace = False)
+            actual = self.actual[ind]
+        else:
+            actual = self.actual
+
+            
+        ###Create noise    
+        self.noise = self.noise_function(len(actual))
+        ###Generate from noise
+        self.generated = self.generator.predict(self.noise)
+        ###Reshape data if necessary
+        if self.actual_reshape != None: 
+            self.generated = self.generated.reshape(self.actual_reshape)
+        
+        ###Train discriminator
+        
+        ##Create labels
+        labels = np.concatenate(
+            [ np.ones(len(actual))*self.smoothing ,
+             np.zeros(len(self.noise)) ]).reshape(-1,1)
+        
+        ##Create X joining actual and generated
+        x = np.concatenate([actual, self.generated])
+        
+        ###Train discriminator
+        self.discriminator.trainable = True
+        if self.verbose == 1: print('Training Discriminator:')
+        self.discriminator.fit(
+            x = x,
+            y = labels,
+            verbose = self.verbose,
+            epochs = self.epochs_discr,
+            batch_size = self.batch_size_disc
+            )
+        #Append discriminator loss
+        self.loss_disc.append(self.discriminator.history.history['loss'])
+        
+        ###Select from actual (batch size)
+        if self.size_gen != None:
+            ind = np.random.choice(
+                range(len(self.actual)),self.size_gen, replace = False)
+            actual = self.actual[ind]
+        else:
+            actual = self.actual
+            
+        ###Create noise    
+        self.noise = self.noise_function(len(actual))
+        
+        ###Train generator
+        if self.verbose == 1: print('Training Generator:')
+        self.discriminator.trainable = False
+        self.generator.trainable = True
+        
+        ###Fit GAN model
+        self.gan.fit(self.noise, np.ones((len(self.noise),1)),
+                     epochs = self.epochs_gen, verbose = self.verbose,
+                    batch_size = self.batch_size_gen)
+        #Append generator loss
+        self.loss_disc.append(self.gan.history.history['loss'])
+        
+        self.round += 1
+        
+    def predict_gan(self,noise):
+        
+        return self.generator.predict(noise)
+    
+    def train_batch(self,
+                  noise_function,
+                  actual,
+                  n_do = 1,
+                  smoothing = 1,
+                  verbose = 1,
+                  actual_reshape = None,
+                  epochs_discr = 1,
+                  epochs_gen = 1,
+                  size_disc = None,
+                  batch_size_disc = None,
+                  size_gen = None,
+                  batch_size_gen = None,
+                  n_show = None,
+                  show_reshape = None,
+                  cmap = None,
+                  grid_show = (2,2)
+                    
+                   ):
+        
+        self.__dict__.update(locals())        
+        
+        for i in range(self.n_do):
+        
+            self.train_gan(
+                  noise_function = self.noise_function,
+                  actual = self.actual,
+                  smoothing = self.smoothing,
+                  verbose = self.verbose,
+                  actual_reshape = self.actual_reshape,
+                  epochs_discr = self.epochs_discr,
+                  epochs_gen = self.epochs_gen,
+                  size_disc = self.size_disc,
+                  batch_size_disc = self.batch_size_disc,
+                  size_gen = self.size_gen,
+                  batch_size_gen = self.batch_size_gen,
+            )
+            
+            if n_show != None:
+                
+                if i % n_show == 0:
+                
+                    print('Round: ',self.round)
+
+                    noise = self.noise_function(np.prod(grid_show))
+
+                    generated = self.predict_gan(noise)
+
+                    plot_grid(generated.reshape(show_reshape),
+                                   self.grid_show,cmap = self.cmap)
+                    
+
+        
+
         
         
+
+
         
         
         
